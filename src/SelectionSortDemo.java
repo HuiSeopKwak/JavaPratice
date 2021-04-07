d5ccbc0054cf3b1c57c3f751ab9de20c9ea61ef1
user <user@LAPTOP-0B6VMU37>
2021-04-07 20:29:15 +0900

    .

--- src/SelectionSortDemo.java
+++ src/SelectionSortDemo.java
@@ -1,9 +1,35 @@
+import java.util.Scanner;
 
 public class SelectionSortDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		PartiallyFilledArray pfa = new PartiallyFilledArray(10);
+		System.out.println("Enter numbers to be sorted "
+				+ "(For stop, enter a negative number):");
+		Scanner keyboard = new Scanner(System.in);
+		int a = 0;
+		while(check(a)) {
+			double point = keyboard.nextDouble();
+			if (point < 0)
+				break;
+			pfa.add(point);
+			a++;
+		}
+		pfa.selectionSort();
+		
+		System.out.println("Sorted array values:");
+		while(check(a)) {
+			System.out.println(pfa.getElement(a))
+			a--;
+		}
+		keyboard.close();
+	}
+	
+	public static boolean check(int a) {
+		boolean result = true;
+		if (a > 9 || a < 0)
+			result = false;
+		return result;
 	}
 
 }
